f4fafe0ffaab09f6a498a0ef1a125198b3ef731d
carryandhe <2300899418@qq.com>
2023-11-20 23:59:56 +0800

    学习C语言

---
+++ C-Language/extern.c
@@ -0,0 +1,12 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#pragma warning(disable:6031)
+
+//static int g_val = 2023;//静态的全局变量
+//int g_val = 2023;//全局变量
+
+//static int Add(int x, int y)		//具有内部链接属性  不可以使用
+//int Add(int x, int y)				//具有外部链接属性	可以使用
+//{
+//	int z = x + y;
+//	return z;
+//}
